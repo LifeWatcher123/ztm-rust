499a48fbd18247c9232217501615fd7f9e2cccaa
Christian Aranas <aranaschristianlouise@gmail.com>
2023-07-09 00:17:45 +0800

    Completion of activity a14 - Strings
    Signed-off-by: Christian Aranas <aranaschristianlouise@gmail.com>

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,43 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String,
+}
+
+impl Person {
+    fn print_info(&self) {
+        println!(
+            "Name: {:?}; Favorite Color/s: {:?}",
+            self.name, self.fav_color
+        );
+    }
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            age: 8,
+            name: String::from("Alex"),
+            fav_color: String::from("Red"),
+        },
+        Person {
+            age: 5,
+            name: String::from("Bryan"),
+            fav_color: String::from("Yellow"),
+        },
+        Person {
+            age: 25,
+            name: String::from("Audra"),
+            fav_color: String::from("Orange"),
+        },
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            person.print_info();
+        }
+    }
+}
