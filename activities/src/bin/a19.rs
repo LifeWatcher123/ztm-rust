62416730973894c179f5a5b452a662037ff2e07a
Christian Aranas <aranaschristianlouise@gmail.com>
2023-07-09 22:50:05 +0800

    Completion of activity a19 - Hashmap
    Signed-off-by: Christian Aranas <aranaschristianlouise@gmail.com>

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,4 +13,30 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut furniture_stock: HashMap<_, i32> = HashMap::new();
+
+    furniture_stock.insert("Chair".to_string(), 5);
+    furniture_stock.insert("Bed".to_string(), 3);
+    furniture_stock.insert("Table".to_string(), 2);
+    furniture_stock.insert("Couch".to_string(), 0);
+
+    let mut total_stock = 0;
+
+    // Apparently, you need to use reference here when comparing with an if block. Should research.
+    for (furniture, stock) in &furniture_stock {
+        // Alternatively, it seems that * helps. Should research.
+        // Crash course used a macro named `format!` to store a variable to a string literal??
+        if *stock == 0 {
+            println!("Furniture: {:?}, Stock: <out of stock>", furniture);
+        } else {
+            println!("Furniture: {:?}, Stock: {:?}", furniture, stock);
+        };
+
+        total_stock += stock;
+    }
+
+    println!("\nTotal Stock: {:?}", total_stock);
+}
