f8f404ed8dacd758790605b300113e405465402f
Christian Aranas <aranaschristianlouise@gmail.com>
2023-07-09 20:25:48 +0800

    Completion of activity a16 - Options
    Signed-off-by: Christian Aranas <aranaschristianlouise@gmail.com>

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,4 +8,32 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+
+struct StudentLocker {
+    student_name: String,
+    locker_num: Option<i32>,
+}
+
+fn main() {
+    let student_locker = StudentLocker {
+        student_name: "Maria".to_owned(),
+        locker_num: None,
+    };
+
+    match student_locker.locker_num {
+        Some(locker_num) => {
+            println!(
+                "Student Name: {:?} | Locker: {:?}",
+                student_locker.student_name, locker_num
+            )
+        }
+        None => {
+            println!(
+                "Student Name: {:?} | Locker: None",
+                student_locker.student_name
+            )
+        }
+    }
+}
